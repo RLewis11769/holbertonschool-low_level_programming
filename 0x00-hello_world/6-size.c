9eb453b8beba131c13b9b8e1f649fcfe2c6baf37
RLewis11769 <RLewis11769@gmail.com>
2021-02-04 04:50:32 +0000

    Add comments to 6-size.c file

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,25 @@
 #include <stdio.h>
-int main()
+int main(void)
+/**
+ * main - Prints series of strings
+ * c - character data type
+ * i - integer data type
+ * l - long data type
+ * ll - long long data type
+ * f - float data type
+ * Return : Returns the strings
+ */
 {
-  char c;
-  int i;
-  long l;
-  long long ll;
-  float f;
+char c;
+int i;
+long l;
+long long ll;
+float f;
 printf("Size of a char: %lu byte(s)\n", sizeof(char));
 printf("Size of an int: %lu byte(s)\n", sizeof(i));
- printf("Size of a long int: %lu byte(s)\n", sizeof(l));
- printf("Size of a long long int: %lu byte(s)\n", sizeof(ll));
- printf("Size of a float: %lu byte(s)\n", sizeof(f));
-return(0);
+printf("Size of a long int: %lu byte(s)\n", sizeof(l));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(ll));
+printf("Size of a float: %lu byte(s)\n", sizeof(f));
+return (0);
 }
 
