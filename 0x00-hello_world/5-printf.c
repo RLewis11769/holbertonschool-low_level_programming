f132ccb6e4282b57a9a8cc120a92a181e3b91a1f
RLewis11769 <RLewis11769@gmail.com>
2021-02-04 03:54:07 +0000

    Add data to 5-printf.c file

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Prints string
+ * Return: Returns the string followed by a newline
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
