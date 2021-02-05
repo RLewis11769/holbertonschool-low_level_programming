8ac56f9afdecdf14884e7d90de3b4e0d3311e0ad
RLewis11769 <RLewis11769@gmail.com>
2021-02-05 05:49:19 +0000

    Add data to 9-print_comb.c file

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
 
 /**
- * main - 
- * Return: 
+ * main - Prints 0 to 9 separated by commas
+ * Return: Returns comma-separated string
  */
 
 int main(void)
 {
- return 0;
+int num;
+num = 0;
+while (num < 10)
+{
+num++;
+putchar(num + '0');
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+return (0);
 }
