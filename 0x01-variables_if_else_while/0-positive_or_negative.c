23522a2cff3df0f9dbbb859e7dfa0eec4f38776e
RLewis11769 <RLewis11769@gmail.com>
2021-02-05 01:21:19 +0000

    Add data to 0-positive_or _negative file

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main -
+ * Return:
+ */
+
+int main(void)
+{
+int n;
+scanf("%d", &n);
+
+if (n > 0)
+  printf("%d is positive\n", n);
+else if (n == 0)
+  printf("%d is zero\n", n);
+else
+  printf("%d is negative\n", n);
+
+return (0);
+}
