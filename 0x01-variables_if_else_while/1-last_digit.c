542071eb32c4d45e45ba0e7a9ddfc768ad35f6cb
RLewis11769 <RLewis11769@gmail.com>
2021-02-05 04:59:03 +0000

    Correct format of 1-last_digit file

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,25 +3,24 @@
 #include <stdio.h>
 
 /**
- * main - 
- * Return: 
+ * main - Prints number and its last digit and string
+ * Return: Returns string based on last digit of string
  */
 
 int main(void)
 {
-  int n, lastdigit;
+int n, lastdigit;
 
-  srand(time(0));
-  n = rand() - RAND_MAX / 2;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
 
-  lastdigit = abs(n % 10);
-  printf("Last digit of %d is %d ", n, lastdigit);
-  if (lastdigit > 5)
-    printf("and is greater than 5\n");
-	   else if (lastdigit == 0)
-	   printf("and is 0\n");
-	   else
-	     printf("and is less than 6 and not 0\n");
-
- return 0;
+lastdigit = abs(n % 10);
+printf("Last digit of %d is %d ", n, lastdigit);
+if (lastdigit > 5)
+printf("and is greater than 5\n");
+else if (lastdigit == 0)
+printf("and is 0\n");
+else
+printf("and is less than 6 and not 0\n");
+return (0);
 }
