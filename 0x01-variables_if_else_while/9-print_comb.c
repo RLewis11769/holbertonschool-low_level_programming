56f62e4787cceed7dcb82616781aba1a8622a883
RLewis11769 <RLewis11769@gmail.com>
2021-02-05 21:51:38 +0000

    Correct data in 9-print_comb.c file

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,20 +2,22 @@
 
 /**
  * main - Prints 0 to 9 separated by commas
- * Return: Returns comma-separated string
+ * Return: Returns 0 if successful
  */
 
-int main(void)
-{
+int main(void){
 int num;
-num = 0;
-while (num < 10)
+
+for (num = 0; num < 10; num++)
 {
-num++;
 putchar(num + '0');
+if (num < 9)
+{
 putchar(',');
 putchar(' ');
 }
+}
 putchar('\n');
+
 return (0);
 }
