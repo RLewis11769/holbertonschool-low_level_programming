a03f62fe99f84f5ef9f439451b6438749df85763
RLewis11769 <RLewis11769@gmail.com>
2021-02-05 18:51:29 +0000

    Add data to 101-quote.c file

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Prints string
+ * Return: Returns the string followed by a newline
+ */
+int main(void)
+{
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
+return (0);
+}
