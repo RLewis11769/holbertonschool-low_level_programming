6d142d1ead18f625d471141ee0d0154e47a69dd7
RLewis11769 <RLewis11769@gmail.com>
2021-02-05 01:26:37 +0000

    Add basic data to 0-positive_or_negative file

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
 
 /**
@@ -7,8 +9,10 @@
 
 int main(void)
 {
-int n;
-scanf("%d", &n);
+  int n;
+
+  srand(time(0));
+  n = rand() - RAND_MAX / 2;
 
 if (n > 0)
   printf("%d is positive\n", n);
