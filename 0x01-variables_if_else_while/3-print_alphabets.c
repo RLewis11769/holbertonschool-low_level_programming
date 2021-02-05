7e3310e6a0d664314246f48ad177002f2a0bad5d
RLewis11769 <RLewis11769@gmail.com>
2021-02-05 05:04:31 +0000

    Correct format of 3-print_alphabet.c file

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
 /**
- * main - 
- * Return: 
+ * main - Prints alphabet in lowercase then uppercase
+ * Return: Returns 1 string
  */
 
 int main(void)
 {
-  int let;
-  for(let = 'a'; let <= 'z'; let++)
-    putchar(let);
-  for(let = 'A'; let <= 'Z'; let++)
-    putchar(let);
-  putchar('\n');
- return 0;
+int let;
+for (let = 'a'; let <= 'z'; let++)
+putchar(let);
+for (let = 'A'; let <= 'Z'; let++)
+putchar(let);
+putchar('\n');
+return (0);
 }
