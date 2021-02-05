7a75473f7bda42711d45fac2cf175ead1ff97e49
RLewis11769 <RLewis11769@gmail.com>
2021-02-05 05:10:09 +0000

    Correct format of 5-print_numbers.c file

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 
 /**
- * main - 
- * Return: 
+ * main - Prints 0 to 9
+ * Return: String of numbers
  */
 
 int main(void)
 {
-  int i;
-  i = 0;
-  while(i < 10)
-    {
-      printf("%d", i);
-      i++;
-    }
-  putchar('\n');
- return 0;
+int i;
+i = 0;
+while (i < 10)
+{
+printf("%d", i);
+i++;
+}
+putchar('\n');
+return (0);
 }
