34387a7327992e21bb972ae0dfc413d910728049
RLewis11769 <RLewis11769@gmail.com>
2021-02-05 02:09:25 +0000

    Add data to 1-last_digit.c file

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
 
 /**
@@ -7,5 +9,20 @@
 
 int main(void)
 {
+  int n;
+unsigned int lastdigit;
+
+  srand(time(0));
+  n = rand() - RAND_MAX / 2;
+
+  lastdigit = abs(n % 10);
+  printf("Last digit of %d is %d ", n, lastdigit);
+  if (lastdigit > 5)
+    printf("and is greater than 5\n");
+	   else if (lastdigit == 0)
+	   printf("and is 0\n");
+	   else
+	     printf("and is less than 6 and not 0\n");
+
  return 0;
 }
