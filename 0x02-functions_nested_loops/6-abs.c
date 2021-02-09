c022c707772a407477bf02407c90761ccd069711
Rachel Lewis <RLewis11769@gmail.com>
2021-02-09 15:43:32 +0000

    Add data to 6-abs.c file and update holberton.h header file

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * _abs - computes absolute value of integer
+ * Return: absolute value
+ * @int: first operand
+ */
+
+int _abs(int r)
+{
+	if (r > 0)
+	{
+		return (r);
+	}
+	else
+	{
+		return (r * -1);
+	}
+}
