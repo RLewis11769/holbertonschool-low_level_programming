27a926d348262abba0c51341ce1df5998128ec33
Rachel Lewis <RLewis11769@gmail.com>
2021-02-09 18:01:46 +0000

    Correct data in 7-print_last_digit.c file

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,13 +8,13 @@
 
 int print_last_digit(int r)
 {
+	int x = (r % 10);
+
 	if (r >= 0)
 	{
-		_putchar(r % 10);
+		return (x);
 	}
 	else
-	{
-		_putchar((r * -1) % 10);
-	}
+		return (x * -1);
 	return (0);
 }
