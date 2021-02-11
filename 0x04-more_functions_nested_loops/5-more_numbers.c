772ce434bfa302183d6624adcce1c7f548de214c
Rachel Lewis <RLewis11769@gmail.com>
2021-02-11 19:48:15 +0000

    Add data to 5-more_numbers file

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,3 +1,5 @@
+#include "holberton.h"
+
 /**
  * more_numbers - prints 0-14 10 times
  *
@@ -6,5 +8,23 @@
 
 void more_numbers(void)
 {
+	int i;
+	int c;
 
+	for (i = 0; i < 10; i++)
+	{
+		for (c = 1; c < 15; c++)
+		{
+			if (c < 10)
+			{
+				_putchar(c + '0');
+			}
+			else
+			{
+			_putchar(c / 10 + '0');
+			_putchar(c % 10 + '0');
+			}
+		}
+		_putchar('\n');
+	}
 }
