4394d5f1eb0f308e04225752e91cd57a9c92b3c9
Rachel Lewis <RLewis11769@gmail.com>
2021-02-11 16:15:51 +0000

    Add data to 0-isupper.c file and corrected header file

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,10 +2,13 @@
  * _isupper - checks for uppercase letters
  * @c: integer to check
  *
- * Return: 1 is uppercase, 0 otherwise
+ * Return: 1 if uppercase, 0 otherwise
  */
 
 int _isupper(int c)
 {
-
+	if (c > 64 && c < 91)
+		return (1);
+	else
+		return (0);
 }
