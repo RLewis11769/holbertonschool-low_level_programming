0e621f126ba56519d82735fdd84836e723bed12b
Rachel Lewis <RLewis11769@gmail.com>
2021-02-12 05:38:07 +0000

    Add data to 7-print_diagonal file

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,3 +1,5 @@
+#include "holberton.h"
+
 /**
  * print_diagonal - draws diagonal line in terminal
  * @n: number of times \ is printed
@@ -7,5 +9,22 @@
 
 void print_diagonal(int n)
 {
+	int d, a;
 
+	if (n > 0)
+	{
+		for (d = 0; d < n; d++)
+		{
+			for (a = 0; a < n; a++)
+			{
+				if (a == d)
+					_putchar('\\');
+				else
+					_putchar(' ');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
 }
