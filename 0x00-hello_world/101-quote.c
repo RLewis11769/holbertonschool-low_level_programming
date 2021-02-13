c956a5607dd5058deb061592e93bc85f26b27862
Rachel Lewis <RLewis11769@gmail.com>
2021-02-13 20:26:45 +0000

    Modify data in 1010-quote.c file

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
- * main - Prints string
- * Return: Returns the string followed by a newline
+ * main - Prints string to stderr
+ * Return: 1 if successful
  */
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-return (1);
-return (0);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
