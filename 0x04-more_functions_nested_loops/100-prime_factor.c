523d7f258f35a5b62143cc674d491b424d9d0973
Rachel Lewis <RLewis11769@gmail.com>
2021-02-13 04:49:50 +0000

    Add structure for advanced tasks

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,19 @@
+/**
+ * main - calls prime_factor function
+ * Return: 0 if successful
+ */
+
+int main(void)
+{
+	prime_factor();
+	return (0);
+}
+
+/**
+ * prime_factor - finds largest prime factor of 612852475143
+ */
+
+void prime_factor(void)
+{
+
+}
---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,20 @@
+/**
+ * main - calls print_number function
+ * Return: 0 if successful
+ */
+
+int main(void)
+{
+	print_number();
+	return (0);
+}
+
+/**
+ * print_number - prints any integer
+ * @n: integer to be printed
+ */
+
+void prime_factor(int n)
+{
+
+}
