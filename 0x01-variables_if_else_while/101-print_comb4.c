dc495071b88e5e757759b195477534c1870fcf29
Rachel Lewis <RLewis11769@gmail.com>
2021-02-14 17:11:01 +0000

    Add data to 101-print_comb4.c file

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Prints lowest possible combination between 0-999
+ * Return: 0 if successful
+ */
+
+int main(void)
+{
+	int d, f, s, t;
+
+	for (d = 0; d < 1000; d++)
+	{
+		f = (d / 100);
+		s = (d / 10 % 10);
+		t = (d % 10);
+		if ((f < s) && (s < t))
+		{
+			putchar(f + '0');
+			putchar(s + '0');
+			putchar(t + '0');
+			if (d < 789)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
