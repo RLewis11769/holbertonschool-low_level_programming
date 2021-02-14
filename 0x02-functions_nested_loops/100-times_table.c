2432d8e5cda34a9eeec55bb69d84deffe6817901
Rachel Lewis <RLewis11769@gmail.com>
2021-02-14 22:03:44 +0000

    Incorrect spacing

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,35 +1,42 @@
 #include "holberton.h"
-/**
- * times_table - writes the character c to stdout
- */
-void times_table(void)
-{
-	int a, b, x, y;
-	int z = 0;
 
-	for (x = 0; x < 10; x++)
+/**
+ * print_times_table - prints n times table
+ * @n: number between 0 and 15 to do times table of
+ * Return: 0 if successful
+ */
+
+void print_times_table(int n)
+{
+	int d, a;
+
+	if ((n > 0) && (n < 16))
 	{
-		for (y = 0; y < 10; y++)
+		for (d = 0; d <= n; d++)
 		{
-			a = z / 10;
-			b = z % 10;
-			if (z > 9)
+			for (a = 0; a <= n; a++)
 			{
-				_putchar(a + '0');
+				if (a * d >= 100)
+					_putchar((a * d) / 100 + '0');
+				if (a * d >= 10)
+					_putchar((a * d) / 10 % 10 + '0');
+				_putchar((a * d) % 10 + '0');
+				if (a < n)
+				{
+					_putchar(',');
+					if (((a * d) >= 0) && ((a * d) <= 9))
+					{
+						_putchar(' ');
+						_putchar(' ');
+					}
+					if (((a * d) >= 10) && ((a * d) < 100))
+					{
+						_putchar(' ');
+					}
+					_putchar(' ');
+				}
 			}
-			if ((z < 10) && (y > 0))
-			{
-				_putchar(' ');
-			}
-			_putchar(b + '0');
-			if (y < 9)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-			z = z + x;
+			_putchar('\n');
 		}
-		_putchar('\n');
-		z = 0;
 	}
 }
