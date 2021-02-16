f745a5149027674a98546a0840bcfda6e203aecd
Rachel Lewis <RLewis11769@gmail.com>
2021-02-16 06:19:06 +0000

    Add data to 7-puts_half.c file

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,5 +7,21 @@
 
 void puts_half(char *str)
 {
+	int n;
+	int length = 0;
 
+	while (str[length] != '\0')
+		length++;
+
+	if (length % 2 != 0)
+	{
+		for (n = (length - 1) / 2; n <= length; n++)
+			_putchar(str[n]);
+	}
+	else
+	{
+		for (n = length / 2; n <= length; n++)
+			_putchar(str[n]);
+	}
+	_putchar('\n');
 }
