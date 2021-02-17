b178b5d6c82aef409984ca3aaf5311a61da02af7
Rachel Lewis <RLewis11769@gmail.com>
2021-02-17 02:40:06 +0000

    Add minimum length to n in 8-print_array.c file

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,11 +11,14 @@ void print_array(int *a, int n)
 {
 	int x;
 
+	if (n <= 0)
+		return;
+
 	for (x = 0; x < n; x++)
 	{
 		if (x < n - 1)
 			printf("%d, ", *(a + x));
 		else
-			printf("%d\n", *(a + (n -1)));
+			printf("%d\n", *(a + (n - 1)));
 	}
 }
