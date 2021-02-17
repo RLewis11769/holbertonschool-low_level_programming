339b5fe1bdc2d933326bf24ba8efbfcc983528b1
Rachel Lewis <RLewis11769@gmail.com>
2021-02-17 00:33:05 +0000

    Add data to 5-rev_string.c file

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,5 +7,24 @@
 
 void rev_string(char *s)
 {
+	int length = 1;
+	int x = 0;
+	int y = 0;
+	int c;
 
+	while (s[length] != '\0')
+		length++;
+
+	y = length - 1;
+
+	while (x < y)
+	{
+		c = s[y];
+		s[y] = s[x];
+		s[x] = c;
+		x++;
+		y--;
+	}
+
+	_putchar(s[x]);
 }
