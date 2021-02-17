89a33953f2a1924f15fefc4506047cac526ded9c
Rachel Lewis <RLewis11769@gmail.com>
2021-02-17 01:41:33 +0000

    Add >0 clause to 8-print_array.c file

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,11 +11,14 @@ void print_array(int *a, int n)
 {
 	int x;
 
-	for (x = 0; x < n; x++)
+	while (n > 0)
 	{
-		if (x < (n - 1))
-			printf("%d, ", a[x]);
-		else
-			printf("%d\n", a[x]);
+		for (x = 0; x < n; x++)
+		{
+			if (x < (n - 1))
+				printf("%d, ", a[x]);
+			else
+				printf("%d\n", a[x]);
+		}
 	}
 }
