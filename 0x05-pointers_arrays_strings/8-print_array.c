e5cc990871a03522d7fefb60965bb72401980070
Rachel Lewis <RLewis11769@gmail.com>
2021-02-17 02:00:43 +0000

    Correct data in 8-print_array.c file

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,14 +11,11 @@ void print_array(int *a, int n)
 {
 	int x;
 
-	while (n > 0)
+	for (x = 0; x < n; x++)
 	{
-		for (x = 0; x < n; x++)
-		{
-			if (x < (n - 1))
-				printf("%d, ", a[x]);
-			else
-				printf("%d\n", a[x]);
-		}
+		if (x != n - 1)
+			printf("%d, ", a[x]);
+		else
+			printf("%d\n", a[x]);
 	}
 }
