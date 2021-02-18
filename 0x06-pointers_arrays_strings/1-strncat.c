9928769e8ec86ad60d7bf471bae2504c646a3300
Rachel Lewis <RLewis11769@gmail.com>
2021-02-18 20:13:48 +0000

    Add data to 1-strncat.c file

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -4,11 +4,21 @@
  * *_strncat - concatenates 2 strings
  * @dest: string to be added onto
  * @src: string to be appended to second string
- * @n: number of max btyes from src
+ * @n: max number of bytes in src
  * Return: pointer to dest
  */
 
 char *_strncat(char *dest, char *src, int n)
 {
+	int x;
+	int dlength = 0;
 
+	while (dest[dlength] != '\0')
+		dlength++;
+
+	for (x = 0; x < n; x++)
+		dest[dlength + x] = src[x];
+	dest[dlength + x] = '\0';
+
+	return (dest);
 }
