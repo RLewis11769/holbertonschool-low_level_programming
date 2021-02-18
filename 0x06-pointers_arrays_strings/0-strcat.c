d6fb32a61e582139157645ee850433b9e4ca4497
Rachel Lewis <RLewis11769@gmail.com>
2021-02-18 19:42:06 +0000

    Add data to 0-strcat.c file

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,5 +9,20 @@
 
 char *_strcat(char *dest, char *src)
 {
+	int x;
+	int dlength = 0;
+	int lengths = 1;
+	char *ptr;
 
+	while (src[lengths] != '\0')
+		lengths++;
+
+	while (dest[dlength] != '\0')
+		dlength++;
+
+	for (x = 0; x < lengths; x++)
+		dest[dlength + x] = src[x];
+	ptr = dest;
+
+	return (ptr);
 }
