aa4f71bf45b19f50a93986fa1946b226887c82f3
Rachel Lewis <RLewis11769@gmail.com>
2021-02-20 03:05:16 +0000

    Correct typo in 7-leet.c file

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -10,10 +10,10 @@ char *leet(char *str)
 {
 	int x = 0;
 	int n = 0;
-	char letter[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'L', 'l'};
+	char letter[] = {'A', 'a', 'E', 'e', 'O', 'o', 'T', 't', 'L', 'l'};
 	int num[] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
 
-	while (str[x] != '0')
+	while (str[x] != '\0')
 	{
 		for (n = 0; n <= 9; n++)
 		{
