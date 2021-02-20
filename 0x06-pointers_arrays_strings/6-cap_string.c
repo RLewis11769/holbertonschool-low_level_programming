975b1b24c4358433014b312b244d5551f7dca798
Rachel Lewis <RLewis11769@gmail.com>
2021-02-20 01:11:27 +0000

    Add data to 6-cap_string.c file

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,7 +6,24 @@
  * Return: characters to be capitalized
  */
 
-char *cap_string(char *x)
+char *cap_string(char *str)
 {
+	int a;
+	int c;
+	int arr[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
+	for (a = 0; str[a] != '\0'; a++)
+	{
+		if ((*str >= 'a') && (*str <= 'z'))
+			str[a] = str[a] - 32;
+		for(c = 0; c < 13; c++)
+		{
+			if (str[a] == arr[c])
+			{
+				if ((str[a + 1] >= 'a') && (str[a + 1] <= 'z'))
+					str[a + 1] = str[a + 1] - 32;
+			}
+		}
+	}
+	return (str);
 }
