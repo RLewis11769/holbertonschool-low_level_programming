9fe75874753238eccbdb094157c86c112844b9bc
Rachel Lewis <RLewis11769@gmail.com>
2021-02-20 02:44:44 +0000

    Add data to 7-leet.c file

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -6,7 +6,21 @@
  * Return: encoded character
  */
 
-char *leet(char *x)
+char *leet(char *str)
 {
+	int x = 0;
+	int n = 0;
+	char letter[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'L', 'l'};
+	int num[] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
 
+	while (str[x] != '0')
+	{
+		for (n = 0; n <= 9; n++)
+		{
+			if ((str[x] == letter[n]) && (letter[n] != num[n]))
+				str[x] = num[n];
+		}
+		x++;
+	}
+	return (str);
 }
