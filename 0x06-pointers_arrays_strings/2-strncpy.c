fb0726804b1d7953ffb9017bc7a43f968061389e
Rachel Lewis <RLewis11769@gmail.com>
2021-02-20 02:18:19 +0000

    Modify data in 2-strncpy.c file

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -4,7 +4,7 @@
  * *_strncpy - copies a string
  * @dest: copied string
  * @src: string to be copied
- * @n: number of bytes in src
+ * @n: number of bytes in src to be copied
  * Return: pointer to dest
  */
 
@@ -12,7 +12,7 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int x;
 
-	for (x = 0; x < n; x++)
+	for (x = 0; x < n && src[x] != '\0'; x++)
 		dest[x] = src[x];
 
 	return (dest);
