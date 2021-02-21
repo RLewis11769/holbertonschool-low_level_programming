1ace53bcd16900079ef7447e15aefd7129a9eb99
Rachel Lewis <RLewis11769@gmail.com>
2021-02-21 22:56:38 +0000

    Add data to 8-rot13.c

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -2,11 +2,28 @@
 
 /**
  * *rot13 - encodes string into rot13
- * @x: pointer to character to be encoded
- * Return: encoded character
+ * @str: pointer to string to be encoded
+ * Return: encoded string
  */
 
-char *rot13(char *x)
+char *rot13(char *str)
 {
+	int i = 0;
+	int x = 0;
+	char rot1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot2[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
+	while (str[i] != '\0')
+	{
+		x = 0;
+		while (rot1[x] != '\0')
+		{
+			if (str[i] == rot1[x]){
+				str[i] = rot2[x];
+				break;}
+			x++;
+		}
+		i++;
+	}
+	return (str);
 }
