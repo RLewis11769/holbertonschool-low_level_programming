8ca6f4c21396813a3561c1cbbb855ee993a230c7
Rachel Lewis <RLewis11769@gmail.com>
2021-02-22 01:07:44 +0000

    Correct data in 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,5 +9,22 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	return (*s1 - *s2);
+	int x;
+	int comp = 0;
+	int length = 0;
+
+	while (s1[length] != '\0')
+		length++;
+
+	for (x = 0; x <= length; x++)
+	{
+		if (*(s1 + x) == *(s2 + x))
+			comp = (*(s1 + x) - *(s2 + x));
+		else
+		{
+			comp = (*(s1 + x) - *(s2 + x));
+			break;
+		}
+	}
+	return (comp);
 }
