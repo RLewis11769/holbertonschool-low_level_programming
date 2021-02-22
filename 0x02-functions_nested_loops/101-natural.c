9db1e95a5dc1054ac4c80e357bf55662ebf220fa
Rachel Lewis <RLewis11769@gmail.com>
2021-02-22 11:51:45 +0000

    Add data to 101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,12 +1,28 @@
-#include "holberton.h"
+#include <stdio.h>
 
 /**
- * natural - Prints multiples of 3 or 5 under 1024
+ * main - Prints multiples of 3 or 5 under 1024
  * Return: 0 if successful
- * @x: First operand
  */
 
-void natural(int x)
+int main(void)
 {
 
+	int x = 1;
+	int y;
+	int sum = 0;
+
+	for (; x < 1024; x++)
+	{
+		if ((x % 3 == 0) || (x % 5 == 0))
+		{
+			for (y = 0; ; y++)
+			{
+				sum = sum + x;
+				break;
+			}
+		}
+	}
+	printf("\n", sum);
+	return (0);
 }
