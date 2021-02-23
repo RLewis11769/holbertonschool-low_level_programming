6c1e61af0742f39ebe4b1e3b46e5c4525f7f8b5c
Rachel Lewis <RLewis11769@gmail.com>
2021-02-23 02:19:01 +0000

    Add data to 4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -9,5 +9,30 @@
 
 char *_strpbrk(char *s, char *accept)
 {
+	int x, y;
+	int length = 0;
+	int alen = 0;
 
+	while (s[length] != '\0')
+		length++;
+
+	while (accept[alen] != '\0')
+		alen++;
+
+	for (x = 0; x <= length; x++)
+	{
+		for (y = 0; y <= alen; y++)
+		{
+			if (s[x] == accept[y])
+			{
+				s = &s[x];
+				return (s);
+			}
+			else if (s[x] == length)
+				break;
+			else
+				continue;
+		}
+	}
+	return (s);
 }
