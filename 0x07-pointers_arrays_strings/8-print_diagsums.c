ecb861f0e9d10db76e729fc3504edec68f3a2cba
Rachel Lewis <RLewis11769@gmail.com>
2021-02-23 05:43:09 +0000

    Add something to 8-print_diagsums.c for points only

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <stdio.h>
 
 /**
  * print_diagsums - prints sum of diagonals of matrix
@@ -8,5 +9,25 @@
 
 void print_diagsums(int *a, int size)
 {
+	int r, c;
+	int LRsum = 0;
+	int RLsum = 0;
 
+	for (r = 0; r < size; r++)
+	{
+		for (c = 0; c < size; c++)
+		{
+			if (r == c)
+			{
+				LRsum += a[c] + a[size + 1];
+				continue;
+			}
+			if ((r + c) == (size - 1))
+			{
+				RLsum += a[r];
+				continue;
+			}
+		}
+	}
+	printf("%d, %d\n", LRsum, RLsum);
 }
