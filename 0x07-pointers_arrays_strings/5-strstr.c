7f0a727741abe0c1557697f80b29b9ee94fc779e
Rachel Lewis <RLewis11769@gmail.com>
2021-02-23 02:28:26 +0000

    Add data to 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -9,5 +9,30 @@
 
 char *_strstr(char *haystack, char *needle)
 {
+	int n, h;
+	int hlen = 0;
+	int nlen = 0;
 
+	while (haystack[hlen] != '\0')
+		hlen++;
+
+	while (needle[nlen] != '\0')
+		nlen++;
+
+	for (n = 0; n <= nlen; n++)
+	{
+		for (h = 0; h <= hlen; h++)
+		{
+			if (haystack[h] == needle[n])
+			{
+				haystack = &haystack[h];
+				return (haystack);
+			}
+			else if (haystack[h] == hlen)
+				break;
+			else
+				continue;
+		}
+	}
+	return (haystack);
 }
