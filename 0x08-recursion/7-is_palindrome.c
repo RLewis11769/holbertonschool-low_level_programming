74438a82a98d71ee09919e4f41eb3fac0a860859
Rachel Lewis <RLewis11769@gmail.com>
2021-02-25 20:36:53 +0000

    Correct data in 7-is_palindrome.c

--- 0x08-recursion/7-is_palindrome.c
+++ 0x08-recursion/7-is_palindrome.c
@@ -1,18 +1,31 @@
 #include "holberton.h"
 
+/**
+ * length - calculates length of string
+ * @s: pointer to string to analyze
+ * Return: length of string
+ */
+
 int length(char *s)
 {
 	if (*s == '\0')
 		return (0);
-	else
-		return (1 + length(s + 1));
+	return (1 + length(s + 1));
 }
 
-int pal(char *s, char first, char last)
+/**
+ * pal - determines if string is palindrome
+ * @s: string to check
+ * @first: first integer
+ * @last: last integer
+ * Return: 1 if palindrome, 0 if not
+ */
+
+int pal(char *s, int first, int last)
 {
 	if (last == 0)
 		return (1);
-	if (first != last)
+	if (*(s + first) != *(s + last))
 		return (0);
 	else
 		return (pal(s, first + 1, last - 1));
@@ -26,5 +39,5 @@ int pal(char *s, char first, char last)
 
 int is_palindrome(char *s)
 {
-	return (pal(s, 0, length(s - 1)));
+	return (pal(s, 0, length(s) - 1));
 }
