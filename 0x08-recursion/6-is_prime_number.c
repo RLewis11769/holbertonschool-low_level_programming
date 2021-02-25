677ef27ea12c13de4155f248326565ceba2e5646
Rachel Lewis <RLewis11769@gmail.com>
2021-02-25 03:18:13 +0000

    Add initial data to 6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,6 +1,30 @@
 #include "holberton.h"
 
 /**
+ * prime - recursive to find prime
+ * @p: test operator
+ * @n: number to test
+ * Return: 1 if prime, 0 if not
+ */
+
+int prime(int n, int p)
+{
+	if (n > 0)
+	{
+		if (p == 1)
+			return (1);
+		else
+		{
+			if (n % p == 0)
+				return (0);
+			else
+				return (prime(n, p - 1));
+		}
+	}
+	return (0);
+}
+
+/**
  * is_prime_number - tells if prime number or not
  * @n: number to determine if prime
  * Return: 1 if prime, 0 if not
@@ -8,5 +32,16 @@
 
 int is_prime_number(int n)
 {
+	int p;
 
+	if ((n == 0) || (n == 1))
+		return (0);
+	else
+	{
+		p = prime(n, n / 2);
+		if (p == 1)
+			return (1);
+		else
+			return (0);
+	}
 }
