45ace38f80ad2f7b10c952497baff4cf4b06e67c
Rachel Lewis <RLewis11769@gmail.com>
2021-02-25 05:57:29 +0000

    Add initial data to 7-is_palindrome.c

--- 0x08-recursion/7-is_palindrome.c
+++ 0x08-recursion/7-is_palindrome.c
@@ -1,12 +1,30 @@
 #include "holberton.h"
 
+int length(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	else
+		return (1 + length(s + 1));
+}
+
+int pal(char *s, char first, char last)
+{
+	if (last == 0)
+		return (1);
+	if (first != last)
+		return (0);
+	else
+		return (pal(s, first + 1, last - 1));
+}
+
 /**
  * is_palindrome - determines if string is palindrome or not
  * @s: pointer to string to analyze
  * Return: 1 if palindrome, 0 if not
  */
 
-void _puts_recursion(char *s)
+int is_palindrome(char *s)
 {
-
+	return (pal(s, 0, length(s)));
 }
