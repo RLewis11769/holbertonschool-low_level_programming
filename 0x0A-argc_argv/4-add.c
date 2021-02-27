e509a1897ae2c2ec9214904e0cb72b69c2bdcbb0
Rachel Lewis <RLewis11769@gmail.com>
2021-02-27 23:43:19 +0000

    Add data to 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
 
 /**
- * main - prints sum of 2 numbers
+ * main - prints sum of numbers entered
  * @argc: number of arguments
  * @argv: array of pointers to argument strings
  * Return: 0 if successful
@@ -9,5 +11,22 @@
 
 int main(int argc, char *argv[])
 {
+	int x;
+	int sum = 0;
 
+	if (argc > 1)
+	{
+		for (x = 1; x < argc; x++)
+		{
+			if (isdigit(*(argv[x])))
+				sum += atoi(argv[x]);
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		printf("%d\n", sum);
+	}
+	return (0);
 }
