2880c4e484281ef0bc7586eb89918f8e249ffa01
Rachel Lewis <RLewis11769@gmail.com>
2021-02-28 12:09:08 +0000

    Add data to 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 /**
  * main - multiplies 2 numbers
  * @argc: number of arguments
@@ -9,5 +9,20 @@
 
 int main(int argc, char *argv[])
 {
+	long int x, y;
+	long int result;
 
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (-1);
+	}
+	else
+	{
+		x = atoi(argv[1]);
+		y = atoi(argv[2]);
+		result = x * y;
+	}
+	printf("%ld\n", result);
+	return (0);
 }
