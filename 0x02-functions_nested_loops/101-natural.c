e76e8f5fe7190c68b30f4d75fa5750b8ca4100de
Rachel Lewis <RLewis11769@gmail.com>
2021-03-01 16:32:39 +0000

    Correct data in 101-natual.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,28 +1,20 @@
 #include <stdio.h>
 
 /**
- * main - Prints multiples of 3 or 5 under 1024
+ * main - Prints sum of multiples of 3 or 5 under 1024
  * Return: 0 if successful
  */
 
 int main(void)
 {
-
 	int x = 1;
-	int y;
 	int sum = 0;
 
 	for (; x < 1024; x++)
 	{
 		if ((x % 3 == 0) || (x % 5 == 0))
-		{
-			for (y = 0; ; y++)
-			{
-				sum = sum + x;
-				break;
-			}
-		}
+			sum = sum + x;
 	}
-	printf("\n", sum);
+	printf("%d\n", sum);
 	return (0);
 }
