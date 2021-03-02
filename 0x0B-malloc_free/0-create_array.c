1efd765cf248a7078a1a0d81610a7406603ce6ad
Rachel Lewis <RLewis11769@gmail.com>
2021-03-02 20:47:46 +0000

    Add if statement for malloc returning 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,9 +11,11 @@
 char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
-	char *p = malloc(size * sizeof(char));
+	char *p = (char *)malloc(size * sizeof(char));
 
 	if (size == 0)
+		return (NULL);
+	if (p == 0)
 		return (NULL);
 	for (i = 0; i < size; i++)
 		p[i] = c;
