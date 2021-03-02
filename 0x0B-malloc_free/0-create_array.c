62e8017319b60673c6a2d70aa123f88bfb2d185b
Rachel Lewis <RLewis11769@gmail.com>
2021-03-02 20:41:28 +0000

    Correct sizeof

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,7 +11,7 @@
 char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
-	char *p = malloc(size * sizeof(size));
+	char *p = malloc(size * sizeof(char));
 
 	if (size == 0)
 		return (NULL);
