6d3803407f919163a09efca70489d46aea0bcdc1
Rachel Lewis <RLewis11769@gmail.com>
2021-03-02 20:36:20 +0000

    Add data to 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,5 +10,12 @@
 
 char *create_array(unsigned int size, char c)
 {
+	unsigned int i;
+	char *p = malloc(size * sizeof(size));
 
+	if (size == 0)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		p[i] = c;
+	return (p);
 }
