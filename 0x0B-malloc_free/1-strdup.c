230afa760fad042c7b70ce54c5c50a0abe2363ab
Rachel Lewis <RLewis11769@gmail.com>
2021-03-03 00:49:33 +0000

    Add data to 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -9,5 +9,20 @@
 
 char *_strdup(char *str)
 {
+	unsigned int length = 0;
+	unsigned int x;
+	char *p;
 
+	while (str[length] != '\0')
+		length++;
+
+	p = (char *)malloc(length * sizeof(char) + 1);
+
+	if (str == NULL)
+		return (NULL);
+	if (p == NULL)
+		return (NULL);
+	for (x = 0; str[x] != '\0'; x++)
+		p[x] = str[x];
+	return (p);
 }
