906f410db71ab846974469a6e70b27095df6e010
Rachel Lewis <RLewis11769@gmail.com>
2021-03-03 03:02:42 +0000

    Add conditional for non-2D array

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -10,16 +10,23 @@
 
 int **alloc_grid(int width, int height)
 {
-	int w, h;
-	int **arr = (int **)malloc(width * sizeof(int *));
+	int w, h, length;
+	int *ptr, **arr;
 
-	if (arr == 0)
-		return (NULL);
 	if ((width <= 0) || (height <= 0))
 		return (NULL);
 
-	for (h = 0; h < height; h++)
-		arr[h] = (int *)malloc(height * sizeof(int));
+	length = (sizeof(int *) * width + sizeof(int) * width * height);
+	arr = (int **)malloc(length);
+	ptr = (int *)(arr + width);
+
+	if (arr == 0)
+		return (NULL);
+	if (ptr == 0)
+		return (NULL);
+
+	for (w = 0; w < width; w++)
+		arr[w] = (ptr + width * w);
 
 	for (w = 0; w < width; w++)
 		for (h = 0; h < height; h++)
