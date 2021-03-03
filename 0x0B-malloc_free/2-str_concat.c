7bb97747fd5be6af2034da14e6a2cc1819301752
Rachel Lewis <RLewis11769@gmail.com>
2021-03-03 01:23:33 +0000

    Add data to 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,5 +10,24 @@
 
 char *str_concat(char *s1, char *s2)
 {
+	unsigned int length1 = 0, length2 = 0;
+	unsigned int x, y;
+	char *p;
 
+	while (s1[length1] != '\0')
+		length1++;
+	while (s2[length2] != '\0')
+		length2++;
+
+	p = (char *)malloc((length1 + length2) * sizeof(char) + 2);
+
+	if (p == NULL)
+		return (NULL);
+
+	for (x = 0; s1[x] != '\0'; x++)
+		p[x] = s1[x];
+	for (y = 0; s2[y] != '\0'; y++)
+		p[y + length1] = s2[y];
+
+	return (p);
 }
