07fff74f6007f2571466df41ad18f3aad1bfa79a
Rachel Lewis <RLewis11769@gmail.com>
2021-03-04 02:28:53 +0000

    Rewrite with length in new function

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -2,6 +2,22 @@
 #include <stdlib.h>
 
 /**
+ * length - finds length of string
+ * @s: string to find length of
+ * Return: length of string
+ */
+
+int length(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	return (len);
+}
+
+/**
  * str_concat - concatenates 2 strings
  * @s1: first string
  * @s2: second string
@@ -10,8 +26,8 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	unsigned int length1 = 0, length2 = 0;
-	unsigned int x, y;
+	int x, y;
+	int c = 0;
 	char *p;
 
 	if (s1 == NULL)
@@ -19,23 +35,23 @@ char *str_concat(char *s1, char *s2)
 	if (s2 == NULL)
 		s2 = "";
 
-	while (s1[length1] != '\0')
-		length1++;
-	while (s2[length2] != '\0')
-		length2++;
+	p = (char *)malloc(length(s1) + length(s2) * sizeof(char) + 1);
 
-	p = (char *)malloc((length1 + length2) * sizeof(char) + 2);
-
-	if (p == 0)
-	{
-		free(p);
+	if (p == NULL)
 		return (NULL);
-	}
 
 	for (x = 0; s1[x] != '\0'; x++)
-		p[x] = s1[x];
+	{
+		p[c] = s1[x];
+		c++;
+	}
+
 	for (y = 0; s2[y] != '\0'; y++)
-		p[y + length1] = s2[y];
+	{
+		p[c] = s2[y];
+		c++;
+	}
+	p[c] = '\0';
 
 	return (p);
 }
