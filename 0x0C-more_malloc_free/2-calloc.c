cacd4f6624d4ecc9dd129032eff1a34b3d219353
Rachel Lewis <RLewis11769@gmail.com>
2021-03-04 19:58:20 +0000

    Add data to 2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,5 +9,20 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
+	unsigned int x;
+	int *p;
 
+	if (nmemb == 0)
+		return (NULL);
+	if (size == 0)
+		return (NULL);
+
+	p = malloc(sizeof(size) * nmemb);
+	if (p == 0)
+		return (NULL);
+
+	for (x = 0; x < size; x++)
+		p[x] = 0;
+
+	return (p);
 }
