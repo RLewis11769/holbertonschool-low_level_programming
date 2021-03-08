51f0e88c4c5ba536ffa794d7e33d3201dfa81b8f
Rachel Lewis <RLewis11769@gmail.com>
2021-03-08 02:30:12 +0000

    Make easier to read

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -14,14 +14,12 @@ void print_array(int *a, int n)
 	if (n < 0)
 		return;
 
-	if (n == 0)
-		printf("%d", a[0]);
-
 	for (x = 0; x < n; x++)
 	{
+		printf("%d", a[x]);
 		if (x < n - 1)
-			printf("%d, ", a[x]);
+			printf(", ");
 		else
-			printf("%d\n", a[x]);
+			putchar('\n');
 	}
 }
