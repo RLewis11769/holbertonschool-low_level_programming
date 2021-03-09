88bd872b4804af6ef186a8e74a5cd33fc35d70ab
Rachel Lewis <RLewis11769@gmail.com>
2021-03-09 19:43:59 +0000

    Only include one header guard

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,11 +15,6 @@ typedef struct dog
 	char *owner;
 } dog_t;
 
-#endif
-
-#ifndef PROTOTYPE_H
-#define PROTOTYPE_H
-
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
