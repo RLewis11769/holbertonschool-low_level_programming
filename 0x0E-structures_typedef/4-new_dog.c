d1c65a4eaa61ca9ba8be93d5124325a40f420fa4
Rachel Lewis <RLewis11769@gmail.com>
2021-03-09 17:13:15 +0000

    Rewrite to reflect PLD discussion

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,6 +2,36 @@
 #include "dog.h"
 
 /**
+ * _strdup - copies string
+ * @str: pointer to string to be duplicated
+ * Return: pointer to duplicated string or NULL if insufficient memory
+ */
+
+char *_strdup(char *str)
+{
+	int x;
+	int length = 0;
+	char *p;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[length] != '\0')
+		length++;
+
+	p = malloc(length * sizeof(char) + 1);
+	if (p == 0)
+		return (NULL);
+
+	for (x = 0; x < length; x++)
+		p[x] = str[x];
+	p[x] = '\0';
+
+	return (p);
+}
+
+
+/**
  * new_dog - create new dog
  * @name: dog name
  * @age: dog age
@@ -9,11 +39,30 @@
  * Return: pointer to new dog info
  */
 
-void dog_t *new_dog(char *name, float age, char *owner)
+dog_t *new_dog(char *name, float age, char *owner)
 {
-	void *newDog;
+	struct dog *newDog;
 
 	newDog = malloc(sizeof(struct dog));
 	if (newDog == NULL)
 		return (NULL);
+
+	newDog->name = _strdup(name);
+	if (newDog->name == NULL)
+	{
+		free(newDog);
+		return (NULL);
+	}
+
+	newDog->age = age;
+
+	newDog->owner = _strdup(owner);
+	if (newDog->owner == NULL)
+	{
+		free(newDog->name);
+		free(newDog);
+		return (NULL);
+	}
+
+	return (newDog);
 }
