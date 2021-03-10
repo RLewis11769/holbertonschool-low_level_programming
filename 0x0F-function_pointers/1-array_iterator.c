5e2d9f3b79b1b51605653cb0bead2232be2b1f6c
Rachel Lewis <RLewis11769@gmail.com>
2021-03-10 17:51:24 +0000

    Add data to 1-array_iterator.c

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - executes functions based on arrays given
+ * @array: array passed in
+ * @size: size of each array
+ * @action:  function pointer
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
