99e2b4cc5696f22182c6ef7d70563ab296a5908b
Rachel Lewis <RLewis11769@gmail.com>
2021-03-11 20:53:22 +0000

    Add data to 1-print_numbers.c

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - prints numbers inputted
+ * @separator: string printed between numbers
+ * @n: count of numbers inputted
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int x;
+	va_list(args);
+
+	if (separator == NULL)
+		separator = "";
+
+	va_start(args, n);
+
+	if (n != 0)
+	{
+		for (x = 0; x < n; x++)
+		{
+			printf("%d", va_arg(args, int));
+			if (x < n - 1)
+				printf("%s", separator);
+		}
+	}
+	va_end(args);
+
+	putchar('\n');
+}
