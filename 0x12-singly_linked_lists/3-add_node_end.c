3376450cb2582cfa98ceac5e677247f3ba753342
RLewis11769 <2708@holbertonschool.com>
2021-03-19 02:02:53 +0000

    Add data to 3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "lists.h"
 
 /**
@@ -9,5 +12,28 @@
 
 list_t *add_node_end(list_t **head, const char *str)
 {
+	list_t *newNode;
+	int length = 0;
 
+	if (head == NULL)
+		return (NULL);
+	if (str == NULL)
+		return (NULL);
+
+	newNode = malloc(sizeof(list_t));
+	if (newNode == NULL)
+		return (NULL);
+
+	while (str[length] != '\0')
+		length++;
+
+	newNode->str = strdup(str);
+	newNode->len = length;
+	newNode->next = NULL;
+
+	while (*head != NULL)
+		head = &(*head)->next;
+	*head = newNode;
+
+	return (newNode);
 }
