9c63eb53726628e0d6c785aa003d6b09db085ee0
RLewis11769 <2708@holbertonschool.com>
2021-03-19 01:09:59 +0000

    Add data to 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "lists.h"
 
 /**
@@ -9,5 +12,25 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
+	list_t *newNode;
+	int length = 0;
 
+	if (head == NULL)
+		return (NULL);
+	if (str == NULL)
+		return (NULL);
+
+	newNode = (list_t *)malloc(sizeof(list_t));
+	if (newNode == NULL)
+		return (NULL);
+
+	while (str[length] != '\0')
+		length++;
+
+	newNode->str = strdup(str);
+	newNode->len = length;
+	newNode->next = *head;
+	*head = newNode;
+
+	return (*head);
 }
