ec0d77d43f345417197da6a6e77d22710e01ec12
RLewis11769 <2708@holbertonschool.com>
2021-03-19 14:38:49 +0000

    Add data to 100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * printBefore - prints before main function is executed
+ *
+ */
+
+void __attribute__ ((constructor)) printBefore()
+{
+	printf("You're beat! and yet, you must allow,
+\n I bore my house upon my back!\n");
+}
