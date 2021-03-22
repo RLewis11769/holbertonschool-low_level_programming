07c3849b1c6e8d3fcd573d4f12e1efb16911b3bc
RLewis11769 <2708@holbertonschool.com>
2021-03-22 18:06:55 +0000

    Add data to 5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint2 - frees all elements of linked list and sets head to NULL
+ * @head: pointer to pointer to head
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	while (*head != NULL)
+	{
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
+	}
+	*head = NULL;
+}
