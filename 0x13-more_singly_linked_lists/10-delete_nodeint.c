8d1a1536344b524ef327b1811e89cc50bb7cf1e7
RLewis11769 <2708@holbertonschool.com>
2021-03-23 04:20:05 +0000

    Add initial data to 10-delete_nodeint.c

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - deletes node at given position
+ * @head: pointer to pointer at beginning of linked list
+ * @idx: position where new node should be added
+ * Return: 1 if successful
+ */
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	unsigned int x;
+	listint_t *temp;
+	listint_t *NorNplus;
+
+	if (*head == NULL)
+		return (-1);
+
+	temp = *head;
+
+	if (index == 0)
+	{
+		*head = temp->next;
+		free(temp);
+		return (1);
+	}
+
+	for (x = 0; x < index - 1; x++)
+		temp = temp->next;
+
+	if (index >= x)
+	{
+		free(temp);
+		return (-1);
+	}
+
+	NorNplus = temp->next;
+	temp->next = NorNplus->next;
+	free(NorNplus);
+
+	return (1);
+}
