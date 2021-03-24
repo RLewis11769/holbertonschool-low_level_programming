b916173c9dc85eb21f5a64f91224d69c8e86134d
RLewis11769 <2708@holbertonschool.com>
2021-03-24 04:11:32 +0000

    Add comments and empty list

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,8 +13,11 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int x = 0;
-	listint_t *temp;
-	listint_t *h;
+	listint_t *temp; /* points at new struct at idx with data n */
+	listint_t *h; /* points at position before new struct inserted */
+
+	if (head == NULL)
+		return (NULL);
 
 	temp = malloc(sizeof(listint_t));
 	if (temp == NULL)
@@ -29,20 +32,23 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		*head = temp;
 		return (*head);
 	}
-
 	h = *head;
 
+	/* 0-based index, 1-based index would be idx - 2 */
 	for (x = 0; x < idx - 1; x++)
 	{
+		/* moves h forward until 1 before where should be inserted */
 		h = h->next;
+		/* condition for position to be inserted larger than exists */
 		if (h->next == NULL)
 		{
 			free(temp);
 			return (NULL);
 		}
 	}
-
+	/* points new struct at position after where it should go */
 	temp->next = h->next;
+	/* points struct 1 behind inserted position at new struct */
 	h->next = temp;
 
 	return (temp);
