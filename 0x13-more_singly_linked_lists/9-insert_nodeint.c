4fd78e45f6da1f7ed32d3d32dc47cfdc782c6fd3
RLewis11769 <2708@holbertonschool.com>
2021-03-24 04:12:18 +0000

    Test with *

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,7 +16,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *temp; /* points at new struct at idx with data n */
 	listint_t *h; /* points at position before new struct inserted */
 
-	if (head == NULL)
+	if (*head == NULL)
 		return (NULL);
 
 	temp = malloc(sizeof(listint_t));
