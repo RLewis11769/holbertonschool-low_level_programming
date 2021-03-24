c13fac6df10189ff8434426d53e4bcadc7b5a142
RLewis11769 <2708@holbertonschool.com>
2021-03-24 04:26:10 +0000

    Better way to put it

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -31,7 +31,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	for (x = 0; x < index - 1; x++)
 	{
 		temp = temp->next;
-		if (temp->next == NULL)
+		if (temp == NULL)
 			return (-1);
 	}
 
