35b3082f5f8b6dd722de56c429c6fd3eb9f94fec
RLewis11769 <2708@holbertonschool.com>
2021-03-26 00:57:53 +0000

    Add data to 5-flip_bits.c

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * flip_bits - finds number of bits required to change one number to another
+ * @n: first number
+ * @m: second number
+ * Return: number of bits required to change
+ */
+
+/*
+ * cannot use / or %
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int sum = 0;
+	int x;
+
+	/* traverses all potential 32 digits for differences */
+	for (x = 0; x < 32; x++)
+	{
+		if (((m >> x) & 1) != ((n >> x) & 1))
+			sum++;
+	}
+	return (sum);
+}
