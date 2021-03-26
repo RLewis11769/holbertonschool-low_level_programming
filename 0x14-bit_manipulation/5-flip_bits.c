cf3f6e55ff9ab44cde6a1fe96eb0958ee255b692
RLewis11769 <2708@holbertonschool.com>
2021-03-26 04:20:43 +0000

    Add edge case for being passed a negative number

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -17,8 +17,11 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	int sum = 0;
 	int x;
 
+	if (m > 4294967295 || n > 4294967295)
+		return (63);
+
 	/* traverses all 64 digits for differences */
-	for (x = 0; x <= 64; x++)
+	for (x = 0; x <= 32; x++)
 	{
 		if (((m >> x) & 1) != ((n >> x) & 1))
 			sum++;
