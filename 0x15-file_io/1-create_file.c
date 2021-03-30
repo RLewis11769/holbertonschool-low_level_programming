b9a563c77e3234aa1f6894a11eb6e50f3d9e17e6
RLewis11769 <2708@holbertonschool.com>
2021-03-30 23:49:56 +0000

    Add data to 1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,38 @@
+#include <fcntl.h>
+#include <unistd.h>
+
+/**
+ * create_file - creates file
+ * @filename: name of file to create
+ * @text_content: string to write to file
+ * Return: 1 if successful
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int o_fd, w_fd;
+	int len;
+
+	if (filename == NULL)
+		return (-1);
+	if (text_content == NULL)
+		text_content = "";
+
+	while (filename[len] != '\0')
+		len++;
+
+	/* opens filename as read-write */
+	/* if does not exist, create and truncate */
+	o_fd = open(filename, O_RDWR | O_CREAT | O_TRUNC);
+	if (o_fd < 0)
+		return (-1);
+
+	/* writes text_content of length len to o_fd */
+	w_fd = write(o_fd, text_content, len);
+	if (w_fd < 0)
+		return (-1);
+
+	close(o_fd);
+
+	return (1);
+}
