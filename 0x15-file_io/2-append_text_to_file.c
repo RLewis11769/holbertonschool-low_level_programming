bf3fc829774d717eeabf956f992fd2e3d8d3124d
RLewis11769 <2708@holbertonschool.com>
2021-03-31 00:26:11 +0000

    Add data to 2-append_text_to_file.c

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,37 @@
+#include <fcntl.h>
+#include <unistd.h>
+
+/**
+ * append_text_to_file - appends text to end of file
+ * @filename: name of file to be added to
+ * @text_content: string to be added to file
+ * Return: 1 if successful
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int o_fd, w_fd;
+	int len = 0;
+
+	if (filename == NULL)
+		return (-1);
+	if (text_content == NULL)
+		text_content = "";
+
+	while (text_content[len] != '\0')
+		len++;
+
+	/* opens filename with read-write permission to append */
+	o_fd = open(filename, O_RDWR | O_APPEND);
+	if (o_fd < 0)
+		return (-1);
+
+	/* writes text_content of length len to o_fd */
+	w_fd = write(o_fd, text_content, len);
+	if (w_fd < 0)
+		return (-1);
+
+	close(o_fd);
+
+	return (1);
+}
