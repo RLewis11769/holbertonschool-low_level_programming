07c6fca7c951b9ddcff009f2be0cf62d54a95720
RLewis11769 <2708@holbertonschool.com>
2021-03-31 00:04:29 +0000

    Find correct length

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 	if (text_content == NULL)
 		text_content = "";
 
-	while (filename[len] != '\0')
+	while (text_content[len] != '\0')
 		len++;
 
 	/* opens filename as read-write */
