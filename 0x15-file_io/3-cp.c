70fb17db3962b382ff4c6669719fa711f3b39931
RLewis11769 <2708@holbertonschool.com>
2021-03-31 04:47:37 +0000

    Rearrange 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,70 +1,103 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
 
 /**
- * srcFunction - opens and reads source file
- * @filefrom: argv[1] meaning the source file
- * @buf: buffer
- * Return: return from read syscall to compare later
+ * mallocBuffer - creates buffer to read from src and write to dest
+ * @src_file: name of src file/filefrom/argv[1] where reading from
+ * Return: pointer to buffer
  */
 
-int srcFunction(char *filefrom, char *buf)
+char *mallocBuffer(char *src_file)
 {
-	int src_fd;
-	int r_fd;
+	char *buf;
 
-	src_fd = open(filefrom, O_RDONLY);
+	buf = malloc(sizeof(char) * 1024);
+	if (buf == NULL)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", src_file);
+		exit(99);
+	}
 
-	r_fd = read(src_fd, buf, 1024);
+	return (buf);
+}
 
-	if (src_fd < 0 || r_fd < 0)
+/**
+ * check98 - checks if can read from src
+ * @src_fd: file descriptor for src file
+ * @r_count: number of bytes read
+ * @filefrom: name of src file/filefrom/argv[1]
+ * @buf: buffer
+ */
+
+void check98(int src_fd, int r_count, char *filefrom, char *buf)
+{
+	if (src_fd < 0 || r_count < 0)
 	{
 		dprintf(STDERR_FILENO,
 			"Error: Can't read from file %s\n", filefrom);
 		free(buf);
 		exit(98);
 	}
-
-	return (r_fd);
 }
 
 /**
- * destFunction - opens and writes to destination file
- * @fileto: argv[2] meaning destination file
- * @r_fd: return from source file's read syscall
+ * check99 - checks if can write to dest
+ * @dest_fd: file descriptor for dest file
+ * @w_count: number of bytes written
+ * @r_count: number of bytes read
+ * @fileto: name of dest file/fileto/argv[2]
  * @buf: buffer
  */
 
-void destFunction(char *fileto, int r_fd, char *buf)
+void check99(int dest_fd, int w_count, int r_count, char *fileto, char *buf)
 {
-	int dest_fd;
-	int w_fd;
-
-	dest_fd = open(fileto, O_WRONLY | O_CREAT | O_TRUNC, 0664);
-
-	w_fd = write(dest_fd, buf, r_fd);
-
-	if (dest_fd < 0 || w_fd < 0 || w_fd != r_fd)
+	if (dest_fd < 0 || w_count < 0 || w_count != r_count)
 	{
-		dprintf(STDERR_FILENO,
-			"Error: Can't write to %s\n", fileto);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", fileto);
 		free(buf);
 		exit(99);
 	}
 }
 
 /**
- * main - copies content of one file to another
+ * closeSyscall - closes file indicated by fd
+ * @fd: file descriptor for file to be closed (either src or dest)
+ */
+
+void closeSyscall(int fd)
+{
+	int c_fd;
+
+	c_fd = close(fd);
+
+	if (c_fd < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
+
+/**
+ * main - copies contents of one file to another
  * @argc: number of arguments
  * @argv: array of pointers to argument strings
- * Return: actual number of letters printed
+ * Return: 0 if successful
+ */
+
+/*
+ * Error messages:
+ * 97 - incorrect number of arguments, needs 3 - name, filefrom, fileto
+ * 98 - can't read from argv[1] (read fail, src open fail)
+ * 99 - can't write to argv[2] (write fail, dest open fail, buffer fail)
+ * 100 - can't close file (src close fail, dest close fail)
  */
 
 int main(int argc, char *argv[])
 {
-	int r_fd;
+	int src_fd, dest_fd;
+	int r_count, w_count;
 	char *buf;
 
 	if (argc != 3)
@@ -73,17 +106,25 @@ int main(int argc, char *argv[])
 		exit(97);
 	}
 
-	buf = malloc(sizeof(char) * 1024);
-	if (buf == NULL)
-	{
-		dprintf(STDERR_FILENO,
-			"Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
+	buf = mallocBuffer(argv[1]);
 
-	r_fd = srcFunction(argv[1], buf);
-	destFunction(argv[2], r_fd, buf);
+	/* opens src file and reads to buf */
+	src_fd = open(argv[1], O_RDONLY);
+	r_count = read(src_fd, buf, 1024);
 
+	/* error check if unable to read to buf */
+	check98(src_fd, r_count, argv[1], buf);
+
+	/* opens dest file and writes from buf */
+	dest_fd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	w_count = write(dest_fd, buf, r_count);
+
+	/* error check if unable to write from buf */
+	check99(dest_fd, w_count, r_count, argv[2], buf);
+
+	/* closes both files and frees buffer memory */
+	closeSyscall(src_fd);
+	closeSyscall(dest_fd);
 	free(buf);
 
 	return (0);
