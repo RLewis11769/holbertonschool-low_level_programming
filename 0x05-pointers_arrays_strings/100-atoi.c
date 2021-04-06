580c5f5186e0d5d3caf56b013e04b6ac59e68cff
Rachel Lewis <RLewis11769@gmail.com>
2021-04-06 14:22:14 +0000

    Add data to 100-atoi.c just in case

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,5 +8,23 @@
 
 int _atoi(char *s)
 {
+	int x;
+	int result = 0;
+	int sign = 1;
+	int after = 0;
 
+	for (x = 0; s[x] != '\0'; x++)
+	{
+		if (s[x] == '-')
+			sign = -sign;
+		if (s[x] >= '0' && s[x] <= '9')
+		{
+			result = (result * 10) + (s[x] - '0');
+			after = 1;
+		}
+		else if (after != 0)
+			break;
+	}
+
+	return (sign * result);
 }
