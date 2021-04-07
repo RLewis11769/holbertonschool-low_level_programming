f5f97750483a4ee7c47e9a5b5e1605979dd15c98
Rachel Lewis <RLewis11769@gmail.com>
2021-04-07 03:18:49 +0000

    Rewrite for better readability

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,25 +4,19 @@
  * *_strcmp - compares 2 strings
  * @s1: first string to be compared
  * @s2: second string to be compared
- * Return: number of bytes different
+ * Return: number of bytes different (0 if identical)
  */
 
 int _strcmp(char *s1, char *s2)
 {
 	int x;
 	int comp = 0;
-	int length = 0;
 
-	while (s1[length] != '\0')
-		length++;
-
-	for (x = 0; x <= length; x++)
+	for (x = 0; s1[x] != '\0'; x++)
 	{
-		if (*(s1 + x) == *(s2 + x))
-			comp = (*(s1 + x) - *(s2 + x));
-		else
+		if (s1[x] != s2[x])
 		{
-			comp = (*(s1 + x) - *(s2 + x));
+			comp = (s1[x] - s2[x]);
 			break;
 		}
 	}
