53ed05cbffc98286ead76b4e1f61b44413104135
Rachel Lewis <RLewis11769@gmail.com>
2021-05-02 18:35:53 +0000

    Keep path together

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 gcc -shared -fPIC -o liball.so *.c
-export LD_LIBRARY_PATH=/home/vagrant/holbertonschool-low_level_programming
-/0x18-dynamic_libraries/:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/home/vagrant/holbertonschool-low_level_programming/0x18-dynamic_libraries/:$LD_LIBRARY_PATH
