88327112e1ae5949430af53b5678a0fe17cde51c
Rachel Lewis <RLewis11769@gmail.com>
2021-07-25 18:55:52 +0000

    Add template for 0 - betty not working

---
+++ 0x00-hash_tables/0-hash_table_create.c
@@ -0,0 +1,14 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - Creates hash table
+ * @size: size of array to allocate
+ * Return: Pointer to new hash table or NULL on error
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+    *hash_table_t *ht = NULL;
+
+    return (ht);
+}
