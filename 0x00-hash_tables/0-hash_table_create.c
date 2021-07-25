ed984cddbf34ce03a5d2453c03cfc925c64eb761
Rachel Lewis <RLewis11769@gmail.com>
2021-07-25 20:32:51 +0000

    Add data to 0-hash_table_create.c

--- 0x00-hash_tables/0-hash_table_create.c
+++ 0x00-hash_tables/0-hash_table_create.c
@@ -8,7 +8,25 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    *hash_table_t *ht = NULL;
+    hash_table_t *ht = NULL;
+    unsigned long int i;
+
+    /* Allocate memory for hash table */
+    ht = malloc(sizeof(hash_table_t));
+    if (ht == NULL)
+        return (NULL);
+
+    /* Set value of structs */
+    ht->size = size;
+    ht->array = malloc(sizeof(hash_node_t) * size);
+    if (ht->array == NULL) {
+        free(ht);
+        return (NULL);
+    }
+
+    /* Initialize array to NULL at each index */
+    while (i < size)
+        ht->array[i++] = NULL;
 
     return (ht);
 }
