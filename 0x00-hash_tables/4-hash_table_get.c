982262aeadeac93d60230781bb128e76b5fedbd5
Rachel Lewis <RLewis11769@gmail.com>
2021-07-29 03:31:47 +0000

    Betty and add better comments - don't know why not compiling with last code

--- 0x00-hash_tables/4-hash_table_get.c
+++ 0x00-hash_tables/4-hash_table_get.c
@@ -1,10 +1,10 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_get: Gets value from hash table when given key
+ * hash_table_get - Gets value from hash table when given key
  * @ht: hash table when key/value pair is stored
  * @key: key to search for
- * Return: value associated with key
+ * Return: value associated with key or NULL
  */
 
 char *hash_table_get(const hash_table_t *ht, const char *key)
@@ -13,20 +13,19 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	hash_node_t *match = NULL;
 
 	if (ht == NULL || key == NULL)
-		return NULL;
+		return (NULL);
 
-	/* Get hash value for key/position in array */
+	/* Get hash value/index for key/position in array */
 	pos = key_index((const unsigned char *)key, ht->size);
 	match = ht->array[pos];
 
-	/* Loop through linked list at position until find match */
-	while (match != NULL && match->key != NULL && strcmp( key, match->key ) > 0 )
+	/* Loop through linked list at position until find or end of list */
+	while (match != NULL && match->key != NULL && strcmp(key, match->key) != 0)
 		match = match->next;
-	
-	/* If not found, return NULL */
-	if (match == NULL || match->key == NULL || strcmp( key, match->key ) != 0)
-		return NULL;
-	/* Else return value */
-	else
-		return match->value;
+
+	/* If not found or reach end of list, return NULL */
+	if (match == NULL || match->key == NULL || strcmp(key, match->key) != 0)
+		return (NULL);
+	/* Else strcmp(key, match->key) == 0 */
+	return (match->value);
 }
