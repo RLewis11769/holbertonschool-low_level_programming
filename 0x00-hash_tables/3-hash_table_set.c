26e3f94e601d524592470b66f815c7820f981f51
Rachel Lewis <RLewis11769@gmail.com>
2021-07-29 03:23:34 +0000

    Betty and make sure works

--- 0x00-hash_tables/3-hash_table_set.c
+++ 0x00-hash_tables/3-hash_table_set.c
@@ -1,41 +1,51 @@
 #include "hash_tables.h"
-#include <string.h>
+
+/**
+ * hash_table_set - Add element/node to hash table
+ * @ht: hash table to add values to
+ * @key: key in hash table
+ * @value: value associated with key
+ * Return: 1 if successful, 0 if failure
+ */
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-    unsigned long int idx;
-    hash_node_t *out;
+	unsigned long int idx = 0;
+	hash_node_t *new;
 
-    if (key == NULL || value == NULL || ht == NULL) 
-       return (0);
+	/* Fails if passed bad data */
+	if (key == NULL || value == NULL || ht == NULL)
+		return (0);
+	if (key[0] == '\0')
+		return (0);
 
-    if (key[0] == '\0')
-        return (0);
+	/* Allocate memory for new node */
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
+		return (0);
 
-    out = malloc(sizeof(hash_node_t));
-    if (out == NULL)
-        return (0);
+	/* Duplicate key/value strings bc don't exist outside this function */
+	new->key = strdup(key);
+	if (new->key == NULL)
+	{
+		free(new);
+		return (0);
+	}
 
-    out->key = strdup(key);
-    if (out->key == NULL)
-    {
-        free(out);
-        return (0);
-    }
-    out->value = strdup(value);
+	new->value = strdup(value);
+	if (new->value == NULL)
+	{
+		free(new->key);
+		free(new);
+		return (0);
+	}
 
-    if (out->value == NULL)
-    {
-        free(out->key);
-        free(out);
-        return (0);
-    }
-    
-    idx = key_index((const unsigned char *)key, ht->size);
+	/* Find index of key */
+	idx = key_index((const unsigned char *)key, ht->size);
 
-    out->next = ht->array[idx];
-    ht->array[idx] = out;
+	/* Add new node to hash table */
+	new->next = ht->array[idx];
+	ht->array[idx] = new;
 
-    printf("%s => %lu\n", key, idx);
-    return (1);
+	return (1);
 }
