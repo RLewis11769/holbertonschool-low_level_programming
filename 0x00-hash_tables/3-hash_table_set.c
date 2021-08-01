cf18604d067aa41097d34f9eb6d2728239e58200
Rachel Lewis <RLewis11769@gmail.com>
2021-08-01 21:55:46 +0000

    Rewrite function to work for collisions

--- 0x00-hash_tables/3-hash_table_set.c
+++ 0x00-hash_tables/3-hash_table_set.c
@@ -1,28 +1,20 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_set - Add element/node to hash table
- * @ht: hash table to add values to
- * @key: key in hash table
- * @value: value associated with key
- * Return: 1 if successful, 0 if failure
+ * create_newpair - Creates new key-value pair
+ * @key: Key to new pair
+ * @value: Value to new pair
+ * Return: New pair
  */
 
-int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+hash_node_t *create_newpair(const char *key, const char *value)
 {
-	unsigned long int idx = 0;
 	hash_node_t *new;
-
-	/* Fails if passed bad data */
-	if (key == NULL || value == NULL || ht == NULL)
-		return (0);
-	if (key[0] == '\0')
-		return (0);
 
 	/* Allocate memory for new node */
 	new = malloc(sizeof(hash_node_t));
-	if (new == NULL)
-		return (0);
+	if (!new)
+		return (NULL);
 
 	/* Duplicate key/value strings bc don't exist outside this function */
 	new->key = strdup(key);
@@ -40,12 +32,59 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	}
 
-	/* Find index of key */
-	idx = key_index((const unsigned char *)key, ht->size);
+	/* Set pointer to next node to NULL */
+	new->next = NULL;
+	return (new);
+}
 
-	/* Add new node to hash table */
-	new->next = ht->array[idx];
-	ht->array[idx] = new;
+/**
+ * hash_table_set - Add element/node to hash table
+ * @ht: hash table to add values to
+ * @key: key in hash table
+ * @value: value associated with key
+ * Return: 1 if successful, 0 if failure
+ */
 
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int idx = 0;
+	hash_node_t *new = NULL, *next = NULL, *last = NULL;
+
+	if (key == NULL || value == NULL || ht == NULL || key[0] == '\0')
+		return (0);
+
+	idx = key_index((const unsigned char *)key, ht->size); /* Find index of key */
+	next = ht->array[idx]; /* Set pointer to first node in chain */
+
+	/* If key already exists, overwrite value */
+	if (next && next->key && strcmp(key, next->key) == 0)
+	{
+		free(next->value);
+		next->value = strdup(value);
+	}
+	else /* Else create new node */
+	{
+		if (next == ht->array[idx])
+		{
+			new = create_newpair(key, value);
+			if (new == NULL)
+				return (0);
+			/* Set pointer to new pair */
+			if (next == ht->array[idx])
+			{
+				new->next = next;
+				ht->array[idx] = new;
+			}
+			/* If chain is not empty, set pointer to new pair */
+			else if (next == NULL)
+				last->next = new;
+			/* If chain is empty, set pointer to new pair */
+			else
+			{
+				new->next = next;
+				last->next = new;
+			}
+		}
+	}
 	return (1);
 }
