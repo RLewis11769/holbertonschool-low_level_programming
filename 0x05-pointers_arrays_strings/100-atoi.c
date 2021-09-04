43fb70b8b860ccc3a00e1f7ed60c8ccabd16a109
Rachel Lewis <RLewis11769@gmail.com>
2021-09-04 20:11:42 +0000

    Update atoi to check

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,30 +1,34 @@
-#include "holberton.h"
+#include "main.h"
+#include <stdio.h>
 
 /**
  * _atoi - converts string to integer
- * @s: string to be converted
- * Return: integer that is converted
+ * @s: string where numbers are converted to integers (non-numbers ignored)
+ * Return: integer that is converted or 0 if no conversion is possible
  */
 
 int _atoi(char *s)
 {
-	int x;
+	int c = 0;
 	int result = 0;
-	int sign = 1;
-	int after = 0;
+	int num = 1;
 
-	for (x = 0; s[x] != '\0'; x++)
+	/* First converts to positive or negative based on - */
+	while ((s[c] < '0' || s[c] > '9') && (s[c] != '\0'))
 	{
-		if (s[x] == '-')
-			sign = -sign;
-		if (s[x] >= '0' && s[x] <= '9')
-		{
-			result = (result * 10) + (s[x] - '0');
-			after = 1;
-		}
-		else if (after != 0)
-			break;
+		if (s[c] == '-')
+			num *= -1;
+		c++;
+	}
+	/* If s[c] is a number based on ascii */
+	while ((s[c] >= '0') && (s[c] <= '9'))
+	{
+		/* Includes any previous digits in 10s place (starts as 0) */
+		result = (result * 10);
+		/* Converts to number including pos/neg and adds to result (if any) */
+		result += (num * (s[c] - '0'));
+		c++;
 	}
 
-	return (sign * result);
+	return (result);
 }
