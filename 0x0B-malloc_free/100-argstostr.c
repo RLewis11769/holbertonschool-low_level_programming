3d989c947db32d14d715142c698cd0cfb4ffef0b
Rachel Lewis <RLewis11769@gmail.com>
2021-11-01 23:32:21 +0000

    Change how referencing indices to be less confusing

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -6,7 +6,6 @@
  * @s: string to find length of
  * Return: length
  */
-
 int length(char *s)
 {
 	int len = 0;
@@ -23,50 +22,46 @@ int length(char *s)
  * @av: pointer to argv array
  * Return: pointer to new string, or NULL
  */
-
 char *argstostr(int ac, char **av)
 {
 	int x, y;
-	int s = 0;
+	int index = 0;
 	int len = 0;
 	char *arr = NULL;
 
-	if (ac == 0)
-		return (NULL);
-	if (av == NULL)
+	if ((ac == 0) || (av == NULL))
 		return (NULL);
 
 	/* Find length of each string */
 	for (x = 0; x < ac; x++)
 		len += length(av[x]);
 
-	/* Malloc space for each string and null byte */
-	arr = (char *)malloc(sizeof(char) * len + 1);
+	/* Malloc for each string and null byte */
+	arr = malloc(sizeof(char) * len + 1);
 	if (arr == NULL)
 		return (NULL);
 
-	/* Copy each string into new string */
+	/* Copy each string into new array */
 	for (x = 0; x < ac; x++)
 	{
-		/* Copy each character into new string */
-		for (y = 0; av[x][y] != '\0'; y++)
+		/* For each string, start at 0 index */
+		y = 0;
+		while (av[x][y] != '\0')
 		{
-			arr[s] = av[x][y];
-			s++;
+			/* Copy each character in string into new array */
+			arr[index] = av[x][y];
+			/* Increment in string copying from */
+			y++;
+			/* Increment array string pointer */
+			index++;
 		}
-		/* If not last string, add newline and advance */
-		if (x < ac - 1)
-		{
-			arr[s] = '\n';
-			s++;
-		}
-		/* If last string, add newline and null byte */
-		else
-		{
-			arr[s] = '\n';
-			arr[s + 1] = '\0';
-		}
+		/* Add newline between each string */
+		arr[index] = '\n';
+		/* Increment array for newline character */
+		index++;
 	}
+	/* Add null byte to end of string */
+	arr[index] = '\0';
 
 	return (arr);
 }
