2a57b1ace9a0f50db1538cf98324cf1afb17af70
Rachel Lewis <RLewis11769@gmail.com>
2021-11-02 01:49:56 +0000

    Add main files and comments

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -6,7 +6,6 @@
  * @str: pointer to string to be duplicated
  * Return: pointer to duplicated string or NULL if insufficient memory
  */
-
 char *_strdup(char *str)
 {
 	int x;
@@ -19,17 +18,18 @@ char *_strdup(char *str)
 	while (str[length] != '\0')
 		length++;
 
+	/* Allocate memory for new string and null byte */
 	p = malloc(length * sizeof(char) + 1);
 	if (p == 0)
 		return (NULL);
 
+	/* Copy string, add null byte */
 	for (x = 0; x < length; x++)
 		p[x] = str[x];
 	p[x] = '\0';
 
 	return (p);
 }
-
 
 /**
  * new_dog - create new dog
@@ -38,15 +38,16 @@ char *_strdup(char *str)
  * @owner: dog owner
  * Return: pointer to new dog info
  */
-
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *newDog;
 
+	/* Allocate space for struct */
 	newDog = malloc(sizeof(dog_t));
 	if (newDog == NULL)
 		return (NULL);
 
+	/* Copy name - strings need to be allocated */
 	newDog->name = _strdup(name);
 	if (newDog->name == NULL)
 	{
@@ -54,11 +55,14 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 
+	/* Copy age - ints can be copied directly */
 	newDog->age = age;
 
+	/* Copy owner - strings need to be allocated */
 	newDog->owner = _strdup(owner);
 	if (newDog->owner == NULL)
 	{
+		/* If failure, free name and entire struct */
 		free(newDog->name);
 		free(newDog);
 		return (NULL);
--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -10,6 +10,7 @@ void free_dog(dog_t *d)
 {
 	if (d != NULL)
 	{
+		/* Ints don't need to be freed */
 		free(d->name);
 		free(d->owner);
 		free(d);
